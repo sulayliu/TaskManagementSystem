86c46b66286862c232a38c64e921b1e34a340319
Haris Munir <Haris4000@yahoo.com>
2020-11-09 15:16:08 -0600

    done Comments part .

--- TaskManagementSystem/Models/Note.cs
+++ TaskManagementSystem/Models/Note.cs
@@ -10,6 +10,7 @@ namespace TaskManagementSystem.Models
     {
         public int Id { get; set; }
         public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
         public int ProjectId { get; set; }
         public Project Project { get; set; }
         public int ProTaskId { get; set; }
